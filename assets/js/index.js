d9b93d0807d1cb5bdcc2120697679452b12d5076
Ian Menezes <129297283+ianmenezesss@users.noreply.github.com>
2024-10-18 11:57:23 -0300

    Update final

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,23 @@
-const screenshot = document.querySelectorAll('.screenshot');
-const screenshots = document.querySelector('#screenshots');
-
+const screenshots = document.querySelectorAll('.screenshot img');
+const screenshot = document.querySelectorAll('.screenshot')
 let indexAtual = 0;
-
 
 function mostrarScreenshot(index) {
     screenshot.forEach((screenshot) => screenshot.classList.remove('ativa'));
-
     screenshot[index].classList.add('ativa');
 }
 
-screenshots.addEventListener('click', () => {
-    indexAtual = (indexAtual + 1) % screenshot.length;
-    mostrarScreenshot(indexAtual);
+screenshots.forEach((img, index) => {
+    img.addEventListener('click', () => {
+        indexAtual = (indexAtual + 1) % screenshots.length;
+        mostrarScreenshot(indexAtual);
+    });
+});
+
+
+const menuIcon = document.getElementById('logo-team-cherry');
+const menu = document.getElementById('menu');
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('show');
 });
